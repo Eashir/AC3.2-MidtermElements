90d5d989698432058052c092ae9ccfbe0a353685
Eashir Arafat <eashirarafat@gmail.com>
2016-12-08 16:10:41 -0500

    Added more data

--- AC3.2-MidtermElements/AC3.2-MidtermElements/Element.swift
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/Element.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum elementParseError: Error {
-    case response, name, number, symbol, weight, meltingPoint, boilingPoint
+    case response, name, number, symbol, weight, meltingPoint, boilingPoint, density, crust, discoveryYear, group, electrons, ionEnergy
 }
 
 class Element {
@@ -19,13 +19,35 @@ class Element {
     let weight: Float
     let meltingPoint: Int
     let boilingPoint: Int
-    init(name: String, number: Int, symbol: String, weight: Float, meltingPoint: Int, boilingPoint: Int) {
+    let density: Double
+    let crust: Double
+    let discoveryYear: Int
+    let group: Int
+    let electrons: String
+    let ionEnergy: Float
+    /*
+     "density": 0.09,
+     "crust_percent": 0.14,
+     "discovery_year": "1776",
+     "group": 1,
+     "electrons": "1s1",
+     "ion_energy": 13.5984
+     },
+ */
+    
+    init(name: String, number: Int, symbol: String, weight: Float, meltingPoint: Int, boilingPoint: Int, density: Double, crust: Double, discoveryYear: Int, group: Int, electrons: String, ionEnergy: Float) {
         self.name = name
         self.number = number
         self.symbol = symbol
         self.weight = weight
         self.meltingPoint = meltingPoint
         self.boilingPoint = boilingPoint
+        self.density = density
+        self.crust = crust
+        self.discoveryYear = discoveryYear
+        self.group = group
+        self.electrons = electrons
+        self.ionEnergy = ionEnergy
     }
     
     static func getElements(from data: Data?) -> [Element]? {
@@ -51,11 +73,27 @@ class Element {
                     else { throw elementParseError.meltingPoint }
                 guard let boilingPoint = element["boiling_c"] as? Int
                     else {
-                        let boilingPoint = "Null"
+                        let boilingPoint = 0
                         break
-                }
+                    }
+                guard let density = element["density"] as? Double
+                    else { throw elementParseError.density }
+                guard let crust = element["crust_percent"] as? Double
+                    else { throw elementParseError.meltingPoint }
+                guard let discoveryYear = element["dscovery_year"] as? Int
+                    else {
+                        let discoveryYear = 0
+                        break
+                    }
+                guard let group = element["group"] as? Int
+                    else { throw elementParseError.group }
+                guard let electrons = element["electrons"] as? String
+                    else { throw elementParseError.electrons }
+                guard let ionEnergy = element["ion_energy"] as? Float
+                    else { throw elementParseError.ionEnergy }
                 
-                let validElement = Element(name: name,number: number, symbol: symbol, weight: weight, meltingPoint: meltingPoint, boilingPoint: boilingPoint)
+                
+                let validElement = Element(name: name,number: number, symbol: symbol, weight: weight, meltingPoint: meltingPoint, boilingPoint: boilingPoint, density: density, crust: crust, discoveryYear: discoveryYear, group: group, electrons: electrons, ionEnergy: ionEnergy)
                 elements?.append(validElement)
             }
             return elements
